27c1516c57b9fb160a3598a9d443b9529ca6894c
Felipe4192 <ramos.silva.felipe1410@escola.pr.gov.br>
2026-06-08 13:41:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const botaoSaibaMais = document.getElementById("btn-saiba-mais");
+
+botaoSaibaMais.addEventListener("click", function () {
+    window.open(
+        "https://www.sistemafaep.org.br/agrinho/",
+        "_blank"
+    );
+});
